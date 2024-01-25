262d74e0b987ab953c73a9b1ef301815644b9767
xertifiedloaded <certifiedloaded@yahoo.com>
2024-01-25 17:04:53 +0100

    installation of packages

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const express = require("express");
+const rateLimit = require('express-rate-limit');
+var cors = require('cors')
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const connectDB = require("./src/config/db");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const userRouter = require("./src/routes/app.routes")
 const AdminRouter = require("./src/routes/admin.routes")
-// const cors = require("cors")
+
 dotenv.config();
 PORT = process.env.PORT || 3000;
 
 const app = express();
-// app.use(cors())
+app.use(cors())
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true }));
 app.use('/api/user/v1', userRouter)
 app.use('/api/admin/v1', AdminRouter)
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100, 
+  message: 'Too many requests from this IP, please try again later.',
+});
+app.use(limiter);
 connectDB();
 app.get("/", (req, res) => {
   res.send("This is our application Home Page!!!!😁😁😁");
