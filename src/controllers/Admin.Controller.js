64a2f88a6c1d24417e6f23bf1f60f65bb886a75c
xertifiedloaded <certifiedloaded@yahoo.com>
2024-01-25 16:30:08 +0100

    my changes

--- src/controllers/Admin.Controller.js
+++ src/controllers/Admin.Controller.js
@@ -4,16 +4,22 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 exports.AdminSignUp = async (req, res) => {
     try {
-        const { email, password } = req.body
+        const { email, password, firstName, lastName, phoneNumber } = req.body
         const existingAdmin = await Admin.findOne({ email });
         if (existingAdmin) {
             return errorResMsg(res, 403, "Admin Already Exist")
         }
-        if (!email || !password) {
-            return errorResMsg(res, 500, "Please Enter Email or Password")
+        if (!email || !password || !firstName || !lastName || !phoneNumber) {
+            return errorResMsg(res, 500, "Please fill in all required fields");
         }
         const HashedPassword = await bcrypt.hash(password, 10)
-        const newAdmin = new Admin({ password: HashedPassword, email })
+        const newAdmin = new Admin({
+            email,
+            password: HashedPassword,
+            firstName,
+            lastName,
+            phoneNumber,
+        })
         // generate token to identify each user
         const token = jwt.sign({
             userId: newAdmin._id,
--- src/models/admin.models.js
+++ src/models/admin.models.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
 const AdminSchema = new mongoose.Schema({
+    firstName:String,
+    lastName:String,
     password: String,
+    phoneNumber:String,
     email: { type: String, unique: true },
     role: { type: String, default: "admin" },
     token: { type: String }
